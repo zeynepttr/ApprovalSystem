9f30cfdf39a591ffafcb3399a6d795ea329c4961
zeynep <zeyneptatar71@gmail.com>
2025-12-30 22:18:57 +0300

    dfdgdkl

--- approvalSystem/Gateway/src/main/java/com/gateaway/gateaway/config/SecurityConfig.java
+++ approvalSystem/Gateway/src/main/java/com/gateaway/gateaway/config/SecurityConfig.java
@@ -13,9 +13,9 @@ public class SecurityConfig {
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
     return http
-      .csrf(ServerHttpSecurity.CsrfSpec::disable)
+      .csrf(csrf -> csrf.disable())
       .authorizeExchange(exchange -> exchange
-        .anyExchange().permitAll() // Tüm isteklere koşulsuz izin ver
+        .anyExchange().permitAll()  // Tüm isteklere izin ver
       )
       .build();
   }
